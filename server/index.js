a95dddb7539cd36940e7dc6749fd17559423ec63
Karrissa Volcy <v.karrissa1@gmail.com>
2022-09-16 08:33:27 -0400

    tried to fix heap error

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-
+console.log(__dirname);
 app.listen(process.env.PORT, () => {
   console.log(`server has started on port: ${process.env.PORT}`)
 })
